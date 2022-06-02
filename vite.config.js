21fde1d85c7c44198a996efc61b1e31b5ed5662b
Nick Yeager <nick@quoteapro.com>
2022-06-01 20:06:19 -0700

    update config url

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8080',
-        changeOrigin: true,
+        changeOrigin: false,
       },
     },
   },
